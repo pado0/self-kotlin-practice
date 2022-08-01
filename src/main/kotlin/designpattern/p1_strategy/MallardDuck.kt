d7ea35be1ca694229544f40900852d4a4d754df3
pado0 <bgshhd95@gmail.com>
2022-08-01 23:32:22 +0900

    주석 추가

--- src/main/kotlin/designpattern/p1_strategy/MallardDuck.kt
+++ src/main/kotlin/designpattern/p1_strategy/MallardDuck.kt
@@ -1,7 +1,7 @@
 package designpattern.p1_strategy
 
 class MallardDuck(
-    quackBehavior: QuackBehavior = Quack(), // 특정 클래스에 종속되지만, 일단 이전보다 변경이 쉽긴 한다.
+    quackBehavior: QuackBehavior = Quack(), // 특정 클래스에 종속되지만, 일단 이전보다 변경이 쉽긴 한다. // 생성시 초기화를 해줘야 상속받은 변수가 세팅된다. 혹은 MallardDuck의 생성자에서 변수를 받던가. 그래서 val이 못붙음
     flyBehavior: FlyBehavior = FlyWithWings()
 ) : Duck(
     quackBehavior,
