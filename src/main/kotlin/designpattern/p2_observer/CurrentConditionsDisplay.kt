863c12654df3dfa4e5511f68c727e896582a072d
pado0 <bgshhd95@gmail.com>
2022-08-03 10:20:14 +0900

    observer pattern

--- src/main/kotlin/designpattern/p2_observer/CurrentConditionsDisplay.kt
+++ src/main/kotlin/designpattern/p2_observer/CurrentConditionsDisplay.kt
@@ -1,11 +1,20 @@
 package designpattern.p2_observer
 
-class CurrentConditionsDisplay : Observer, DisplayElement {
-    override fun display() {
+class CurrentConditionsDisplay(
+    val temperature: Long,
+    val humidity: Long,
+    val weatherData: WeatherData
+) : Observer, DisplayElement {
 
+    constructor(weatherData: WeatherData) : this(weatherData = weatherData, temperature = 1, humidity = 1){
+        weatherData.registerObserver(this)
     }
 
     override fun update(temp: Long, humidity: Long, pressure: Long) {
         TODO("Not yet implemented")
     }
+    override fun display() {
+
+    }
+
 }
