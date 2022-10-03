07742890b150081064155093fabaf3f464a347e0
pado0 <bgshhd95@gmail.com>
2022-10-03 23:40:06 +0900

    programmers algo

--- src/main/kotlin/programmers/step1.kt
+++ src/main/kotlin/programmers/step1.kt
@@ -6,6 +6,10 @@ import kotlin.math.sqrt
 fun main() {
     println(p12934(50000000000000))
     println(p12934(3))
+
+    println(p12932(12345).toList())
+    println(p12932_r1(12345).toList())
+    println(p12932_r2(12345).toList())
 }
 
 fun p12934(n: Long): Long {
@@ -16,4 +20,45 @@ fun p12934(n: Long): Long {
     return if (sqrt(n.toDouble()) == sqrt(n.toDouble()).toInt().toDouble()) {
         (sqrt(n.toDouble()).toLong() + 1).toDouble().pow(2).toLong()
     } else -1L
+}
+
+fun p12932(n: Long): IntArray{
+    var t = n
+    var i = 0
+
+    while(t > 0){
+        t /= 10
+        i++
+    }
+
+    val arr = IntArray(i)
+    i = 0
+    t = n
+
+    while(t > 0){
+        arr[i] = (t % 10).toInt()
+        t /= 10
+        i++
+    }
+
+    return arr
+}
+
+fun p12932_r1(n: Long): IntArray{
+
+    var t = n
+    var arr = intArrayOf()
+
+    while(t > 0){
+        arr += (t % 10).toInt() // arr은 더해서 길이를 늘릴 수 있었지
+        t /= 10
+    }
+
+    return arr
+}
+
+fun p12932_r2(n: Long): IntArray{
+
+    return n.toString().reversed().toCharArray().map { it.toInt() - 48 }.toIntArray()
+
 }
