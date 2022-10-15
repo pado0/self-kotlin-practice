8143c95c57493bb46bdd4aca82a5f39efc79a9e9
pado0 <bgshhd95@gmail.com>
2022-10-15 19:48:07 +0900

    programmers algo

--- src/main/kotlin/programmers/level1_2.kt
+++ src/main/kotlin/programmers/level1_2.kt
@@ -2,6 +2,30 @@ package programmers
 
 fun main() {
 
+    print(p12918("1111"))
+    print(p12918 ("a111"))
+}
+
+fun p12950(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+    for( j in 0 until arr1[0].size){ //arr1[0].size ==1
+        for(i in 0 until arr1.size){ //arr1.size = 2
+            arr1[i][j] += arr2[i][j]
+        }
+    }
+
+    return arr1
+}
+
+
+fun p12918_r1(s: String): Boolean{
+    return (s.length == 4 || s.length == 6) && s.toIntOrNull() != null
+}
+
+fun p12918(s: String): Boolean{
+    // 길이가 6일때 알파벳이 두 개 섞여있으면 4가 될 수 있다.
+    return if(s.length == 4) s.filter { it in '0'..'9' }.length == 4
+    else if(s.length == 6) s.filter { it in '0'..'9' }.length == 6
+    else false
 }
 
 fun p12917(s: String): String{
