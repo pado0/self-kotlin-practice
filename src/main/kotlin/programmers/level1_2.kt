bc8f7cbc309ac72dcaf3edf595f8fd390bc43f16
pado0 <bgshhd95@gmail.com>
2022-11-01 09:42:46 +0900

    programmers algo

--- src/main/kotlin/programmers/level1_2.kt
+++ src/main/kotlin/programmers/level1_2.kt
@@ -22,17 +22,60 @@ fun main() {
 //    println(p12926("ab z A ZrZ", 25))
 
 //    println(p12915(arrayOf("sun", "bed", "car"), 1))
-    print(p132267(2,1,20))
-    print(p132267(3, 1, 20))
+//    print(p132267(2,1,20))
+//    print(p132267(3, 1, 20))
+
+//    print(p133499(arrayOf()))
+
+    print(isBabble("a"))
+}
+
+fun p133499(babbling: Array<String>): Int {
+
+    var answer = 0
+    for(i in babbling){
+        if(isBabble(i)) answer++
+    }
+
+    return answer
+}
+
+
+fun isBabble(input: String): Boolean {
+    var str = input
+    var flag = 0
+    while (str.isNotEmpty()) {
+
+        if (str.indexOf("aya") == 0) {
+            if(flag == 1) return false
+            str = str.substring(3 until str.length)
+            flag = 1
+        } else if (str.indexOf("ye") == 0) {
+            if(flag == 2) return false
+            str = str.substring(2 until str.length)
+            flag = 2
+        } else if (str.indexOf("woo") == 0) {
+            if(flag == 3) return false
+            str = str.substring(3 until str.length)
+            flag = 3
+        } else if (str.indexOf("ma") == 0) {
+            if(flag == 4) return false
+            str = str.substring(2 until str.length)
+            flag = 4
+        } else {
+            return false
+        }
+    }
+
+    return true
 }
 
 // sortedWith(compareBy()) 방식도 있다.
-
 //return (if (n > b) n - b else 0) / (a - b) * b // 다음 코드 분석해보기
-fun p132267(a: Int, b: Int, n: Int): Int{
+fun p132267(a: Int, b: Int, n: Int): Int {
     var count = n
     var answer = 0 // 현재 가진 병의 개수
-    while(count > a - 1){
+    while (count > a - 1) {
         answer += count / a * b
         count = count / a * b + (count % a) // 빈병 a개를 n/a번 갖다주면 콜라병 b개가 생김
     }
@@ -41,17 +84,17 @@ fun p132267(a: Int, b: Int, n: Int): Int{
 }
 
 fun p12915(strings: Array<String>, n: Int): Array<String> {
-    return strings.map { it[n] + it }.sorted().map { it.slice(1 until it.length)}.toTypedArray()
+    return strings.map { it[n] + it }.sorted().map { it.slice(1 until it.length) }.toTypedArray()
 }
 
 // 모든 명함을 담을 수 있는 가장 작은 카드지갑의 넓이 구하기
 // 다이나믹 프로그래밍?
-fun p86491(sizes: Array<IntArray>): Int{
+fun p86491(sizes: Array<IntArray>): Int {
 
     var a = 0
     var b = 0
 
-    for(i in sizes){
+    for (i in sizes) {
         a = max(max(i[0], i[1]), a) // 둘중 큰 것 중에 큰 것
         b = max(min(i[0], i[1]), b) // 둘중 작은 것 중에 큰 것
     }
@@ -65,13 +108,12 @@ fun p86491(sizes: Array<IntArray>): Int{
 // 더한 결과가 z를 넘어가면 빼주면 되겠다.
 fun p12926(s: String, n: Int): String {
     return s.map {
-        if(it == ' ') it
-        else if(it in 'a'..'z') {
-            if (it+n in 'a'..'z') it + n
+        if (it == ' ') it
+        else if (it in 'a'..'z') {
+            if (it + n in 'a'..'z') it + n
             else it + n - 26
-        }
-        else{
-            if (it+n in 'A'..'Z') it + n
+        } else {
+            if (it + n in 'A'..'Z') it + n
             else it + n - 26
         }
     }.joinToString("")
@@ -81,6 +123,7 @@ fun p12926(s: String, n: Int): String {
 fun p68935_r1(n: Int): Int {
     return n.toString(3).reversed().toInt(3) // 3진법을 10진수 int로
 }
+
 fun p68935(n: Int): Int {
     // n을 3진법에서 앞뒤로 뒤집은  이를 다시 10진법으로 표현
     var x = n
@@ -88,13 +131,13 @@ fun p68935(n: Int): Int {
     var answer = 0
 
     // 뒤집어진 삼진법
-    while(x  > 0){
+    while (x > 0) {
         y = (y * 10) + (x % 3)
         x /= 3
     }
 
     var i = 0
-    while(y > 0){
+    while (y > 0) {
         answer += ((y % 10) * 3.0.pow(i.toDouble())).toInt()
         y /= 10
         i++
@@ -103,12 +146,11 @@ fun p68935(n: Int): Int {
 }
 
 
-
-fun p12930(s: String): String{
+fun p12930(s: String): String {
     var answer = ""
     var p = 0
     for (i in s.indices) {
-        if(s[i] == ' ') p = 1
+        if (s[i] == ' ') p = 1
         if (p % 2 == 0) answer += s[i].toUpperCase()
         else answer += s[i].toLowerCase()
         p++
@@ -121,34 +163,36 @@ fun p12940_r1(n: Int, m: Int): IntArray {
     val gcd = gcd(n, m)
     return intArrayOf(gcd, n * m / gcd)
 }
+
 tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 
 // 두 수의 최대공약수와 최소공배수
 fun p12940(n: Int, m: Int): IntArray {
-    val mc = (1..max(n,m)).sortedDescending().first { n % it == 0 && m % it == 0 }.toInt()
+    val mc = (1..max(n, m)).sortedDescending().first { n % it == 0 && m % it == 0 }.toInt()
     val mm = mc * (m / mc) * (n / mc)
     return intArrayOf(mc, mm)
 }
 
 fun p12969() {
     val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    (1..b).forEach{(1..a).forEach {print("*")} ; println() }
+    (1..b).forEach { (1..a).forEach { print("*") }; println() }
 }
 
 fun p82612_r1(price: Int, money: Int, count: Int): Long {
     return (1..count).map { it * price.toLong() }.sum().let {
-        if (money > it ) 0 else it - money
+        if (money > it) 0 else it - money
     }
 }
-fun p82612(price: Int, money: Int, count: Int): Long{
+
+fun p82612(price: Int, money: Int, count: Int): Long {
     var m = money.toLong()
-    for(i in 1..count) m -= (price * i)
+    for (i in 1..count) m -= (price * i)
     return if (m < 0) abs(m) else 0L
 }
 
 fun p12950(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-    for( j in 0 until arr1[0].size){ //arr1[0].size ==1
-        for(i in 0 until arr1.size){ //arr1.size = 2
+    for (j in 0 until arr1[0].size) { //arr1[0].size ==1
+        for (i in 0 until arr1.size) { //arr1.size = 2
             arr1[i][j] += arr2[i][j]
         }
     }
@@ -157,18 +201,18 @@ fun p12950(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
 }
 
 
-fun p12918_r1(s: String): Boolean{
+fun p12918_r1(s: String): Boolean {
     return (s.length == 4 || s.length == 6) && s.toIntOrNull() != null
 }
 
-fun p12918(s: String): Boolean{
+fun p12918(s: String): Boolean {
     // 길이가 6일때 알파벳이 두 개 섞여있으면 4가 될 수 있다.
-    return if(s.length == 4) s.filter { it in '0'..'9' }.length == 4
-    else if(s.length == 6) s.filter { it in '0'..'9' }.length == 6
+    return if (s.length == 4) s.filter { it in '0'..'9' }.length == 4
+    else if (s.length == 6) s.filter { it in '0'..'9' }.length == 6
     else false
 }
 
-fun p12917(s: String): String{
+fun p12917(s: String): String {
     return String(s.toCharArray().sortedArrayDescending())
 }
 
